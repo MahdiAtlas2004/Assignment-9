7f7bbebe59f6746acf378fbda251344000cf3280
Mahdi Atlas <mahdiatlas2004@gmail.com>
2023-12-15 09:49:34 +0430

    Q18 added

---
+++ Q18.js
@@ -0,0 +1,43 @@
+var person = {
+    id: 1000,
+    name: "Ahmad",
+    favoriteBooks: [
+        { id: 1, name: "JavaScript", pages: 456 },
+        { id: 2, name: "React.js", pages: 1320 },
+    ],
+    address: { country: "Afghanistan", province: "Herat", district: "Ghurian" },
+    friends: [
+        {
+            id: 1098,
+            name: "Karim",
+            favoriteBooks:
+                [
+                    { id: 1, name: "CSS with Jone", pages: 444 },
+                    { id: 2, name: "React.js", pages: 876 },
+                ],
+            address: {
+                country: "Afghanistan",
+                province: "Herat",
+                district: "Kerokh",
+            },
+        },
+        {
+            id: 1009,
+            name: "Mahmood",
+            favoriteBooks: [
+                { id: 1, name: "Node.js", pages: 765 },
+                { id: 2, name: "Express.js", pages: 2234 },
+            ],
+            address: {
+                country: "Afghanistan",
+                province: "Herat",
+                district: "Qozera",
+            },
+        },
+    ],
+};
+
+//Using loop for to print the favorite books of this person.
+for(let x of person.favoriteBooks){
+    console.log(x);
+}
